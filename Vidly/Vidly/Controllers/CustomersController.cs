0db16bfcb37bd41fad5383e8b2fd53250d211fb5
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-22 20:31:34 +0200

    My first WebSite using ASP.NET MVC- update: DataBase connection using CodeFirst Migrations

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -4,35 +4,91 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private IdentityDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new IdentityDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Cusomers");
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
-        public ActionResult Details(int? id)
+        public ActionResult Edit(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
                 return HttpNotFound();
             }
-            return View(customer);
-        }
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
+
+            var viewModel = new CustomerFormViewModel
             {
-                new Customer { Id = 1, Name = "Clain Rvc" },
-                new Customer { Id = 2, Name = "Larc Glooven" }
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
             };
+
+            return View("CustomerForm", viewModel);
         }
+
+        //public ActionResult Details(int? id)
+        //{
+        //    var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+        //    if (customer == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(customer);
+        //}
     }
 }
--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +10,19 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DataSetDateTime Birthdate { get; set; }
     }
 }
