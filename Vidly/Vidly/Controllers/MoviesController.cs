4646eecea96822991ee17d5be129bdcc620b358c
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-22 17:46:05 +0200

    My first WebSite using ASP.NET - update 3

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,52 +4,82 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie() { Name = "Shrek" };
+            var movies = GetMovies();
 
-            /*ViewData["Movie"] = movie;
-            ViewBag.Movie = movie;
-            */
-
-            return View(movie);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            return View(movies);
         }
 
-        public ActionResult Edit(int id)
+        private IEnumerable<Movie> GetMovies()
         {
-            return Content("id=" + id);
-        }
-
-        // when we navigate to movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
+            return new List<Movie>
             {
-                pageIndex = 1;
-            }
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+                new Movie { Id = 1, Name = "X-man"},
+                new Movie { Id = 2, Name = "Flash"}
+            };
         }
 
-        [Route("movies/released/{year}/{month:regex(\\d{2 }):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+        //    // GET: Movies/Random
+        //    public ActionResult Random()
+        //    {
+        //        var movie = new Movie() { Name = "Shrek" };
+        //        var customers = new List<Customer>
+        //        {
+        //            new Customer {Name = "Customer 1"},
+        //            new Customer {Name = "Customer 2"}
+        //        };
+
+        //        var viewModel = new RandomMovieViewModel
+        //        {
+        //            Movie = movie,
+        //            Customers = customers
+        //        };
+
+        //        /*ViewData["Movie"] = movie;
+        //        ViewBag.Movie = movie;
+        //        */
+
+        //        return View(viewModel);
+
+        //        //return Content("Hello World!");
+        //        //return HttpNotFound();
+        //        //return new EmptyResult();
+        //        //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        //    }
+
+        //    public ActionResult Edit(int id)
+        //    {
+        //        return Content("id=" + id);
+        //    }
+
+        //    // when we navigate to movies
+        //    public ActionResult Index(int? pageIndex, string sortBy)
+        //    {
+        //        if (!pageIndex.HasValue)
+        //        {
+        //            pageIndex = 1;
+        //        }
+
+        //        if (String.IsNullOrWhiteSpace(sortBy))
+        //        {
+        //            sortBy = "Name";
+        //        }
+
+        //        return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //    }
+
+        //    [Route("movies/released/{year}/{month:regex(\\d{2 }):range(1,12)}")]
+        //    public ActionResult ByReleaseDate(int year, int month)
+        //    {
+        //        return Content(year + "/" + month);
+        //    }
+        //}
     }
 }
